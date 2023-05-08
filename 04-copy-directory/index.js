b72cb329d07e9f27c426c6cc38fe3cb8fd18f0bd
RostyslavBaranets <baranets.01@gmail.com>
2023-05-08 19:02:58 +0300

    feat: done 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fs = require('fs').promises;
+const path = require('path');
 
+async function copyDir(){
+    const filesDir = path.join(__dirname, 'files');
+    const filesCopyDir = path.join(__dirname, 'files-copy');
+
+    try{
+        await fs.mkdir(filesCopyDir);
+    } catch (err){
+        if(err.code !== 'EEXIST') throw err;
+    }
+
+    const files = await fs.readdir(filesDir);
+
+    for(const file of files){
+        const currentFile = path.join(filesDir, file);
+        const copyFile = path.join(filesCopyDir, file);
+        const stat = await fs.stat(currentFile);
+
+        if(stat.isDirectory()){
+            await copyDir(currentFile, copyFile);
+        }else{
+            await fs.copyFile(currentFile, copyFile);
+        }
+    }
+    console.log('Файлы скопированы');
+}
+
+copyDir();
